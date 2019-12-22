75c4b2e0025add8ee43dfc9b46d1b819b3ed1d2f
Agrimagsrl <web@agrimag.it>
2019-12-22 11:01:21 +0100

    fix Twitter username

--- assets/app.js
+++ assets/app.js
@@ -21,13 +21,19 @@ jQuery(function() {
                 autocomplete.sources.hits(index, {hitsPerPage: 5})(query, function(suggestions) {
                     var index = {}
 
-                    suggestions = suggestions.filter(function(suggestion) {
-                        if (suggestion.permalink in index)
-                            return false
+                    suggestions = suggestions
+                        .filter(function(suggestion) {
+                            if (suggestion.permalink in index)
+                                return false
 
-                        return (index[suggestion.permalink] = true)
-                    })
+                            return (index[suggestion.permalink] = true)
+                        })
+                        .map(function(suggestion) {
+                            suggestion.permalink = suggestion.permalink.replace('eloquent.blog/', 'eloquentarduino.github.io/')
+                            return suggestion
+                        })
                     
+                    console.log('sugg', suggestions)
                     callback(suggestions)
                 })
             },
@@ -40,7 +46,7 @@ jQuery(function() {
         }
         ])
         .on('autocomplete:selected', function(event, suggestion, dataset) {
-            window.location.href = suggestion.permalink.replace('eloquent.blog/', 'eloquentarduino.github.io/')
+            window.location.href = suggestion.permalink
         });
 
     /*document.querySelector(".searchbox [type='reset']").addEventListener("click", function() {
